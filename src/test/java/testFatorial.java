6bd10379ce3bffbf06bba22a1e067f09044435cf
ekarla <evs@icomp.yfam.edu.br>
2020-11-19 20:53:29 -0400

    test sucess

--- src/test/java/testFatorial.java
+++ src/test/java/testFatorial.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.*;
 public class testFatorial extends calculeFatorial {
 
     @Test
-    public void testFatorial() {
+    public void testFat() {
 
         assertEquals(120,fat(5));
     }
